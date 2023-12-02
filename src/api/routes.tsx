76d2b7b96ec713f294854935c2933c96a7de4c59
Stephano <schang@apptelink.com>
2023-12-02 07:51:21 -0500

    Update routes.tsx

--- src/api/routes.tsx
+++ src/api/routes.tsx
@@ -1,7 +1,7 @@
 export const Endpoints = {
   //BaseURL: 'https://b706-157-100-158-182.ngrok-free.app',
-  BaseURL: 'http://localhost:8000',
-  //BaseURL:'https://victoria-api.up.railway.app',
+  //BaseURL: 'http://localhost:8000',
+  BaseURL:'https://victoria-api.up.railway.app',
   //BaseURL:'http://127.0.0.1:8000',
   Api: "/api",
   login: "/auth/login/",
