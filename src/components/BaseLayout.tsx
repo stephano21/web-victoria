40a56528d9aa3e7b2310f6408a12a7572ebd44de
Stephano <stephanochang21@gmail.com>
2024-01-25 02:08:22 -0500

    FAil

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -1,15 +1,16 @@
-import React, { ReactNode, useContext, useState } from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import Accordion from 'react-bootstrap/Accordion';
-import ListGroup from 'react-bootstrap/ListGroup';
+import React, { ReactNode, useContext } from "react";
+import {
+  Drawer,
+  Button,
+  Typography,
+  IconButton,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
 import banner from './../assets/brand.png'
 //Auth
 import { useAuth } from "./../context/AuthContext";
-import { useOutletContext } from "react-router-dom";
 import { AlertContext } from "../context/AlertContext";
 interface BaseLayoutProps {
   PageName?: string;
@@ -20,133 +21,68 @@ export const BaseLayout: React.FC<BaseLayoutProps> = (
   props: BaseLayoutProps
 ) => {
   const { children, PageName } = props;
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const handleLinkClick = () => setShow(true);
+  const [open, setOpen] = React.useState(false);
+  const openDrawer = () => setOpen(true);
+  const closeDrawer = () => setOpen(false);
+  const [openAcordion, setOpenAcordion] = React.useState(0);
+ 
+  const handleOpen = (value) => setOpenAcordion(open === value ? 0 : value);
   const { alerts } = useContext(AlertContext);
   const { logout, UserData } = useAuth();
   return (
-    <>
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Plant Trace</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Accordion>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header><i className="bi bi-cloud-lightning-rain"></i> &nbsp; Clima</Accordion.Header>
-              <Accordion.Body>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <Nav.Link href="/weather/sync"><i className="bi bi-arrow-repeat"></i>&nbsp;&nbsp;Sincronizaciones</Nav.Link>
-                  </ListGroup.Item>
-
-                </ListGroup>
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header><i className="bi bi-signpost-2"></i> &nbsp;Parametrizaciones</Accordion.Header>
-              <Accordion.Body>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <Nav.Link href="/crop/trees"><i className="bi bi-tree-fill"></i>&nbsp;&nbsp;Plantas</Nav.Link>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Nav.Link href="/crop/lots"><i className="bi bi-map-fill"></i>&nbsp;&nbsp;Lotes</Nav.Link>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Nav.Link href="/crop/production"><i className="bi bi-lightning-charge-fill"></i>&nbsp;&nbsp;Produccion</Nav.Link>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Nav.Link href="/crop/readings"><i className="bi bi-list-check"></i>&nbsp;&nbsp;Lecturas</Nav.Link>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-              <Accordion.Header><i className="bi bi-graph-up-arrow"></i> &nbsp;Estimaciones</Accordion.Header>
-              <Accordion.Body>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <Nav.Link href="/pred/analytics"><i className="bi bi-bar-chart-fill"></i>&nbsp;&nbsp;Estadisticas</Nav.Link>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Nav.Link href="/pred/averange"><i className="bi bi-graph-up"></i>&nbsp;&nbsp;Estimaciones</Nav.Link>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="3">
-              <Accordion.Header><i className="bi bi-person-gear"></i> &nbsp;Administrador</Accordion.Header>
-              <Accordion.Body>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <Nav.Link href="/auth/users"><i className="bi bi-people-fill"></i>&nbsp;&nbsp;Usuarios</Nav.Link>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Nav.Link href="/auth/role"><i className="bi bi-person-fill-lock"></i>&nbsp;&nbsp;Roles</Nav.Link>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </Offcanvas.Body>
-      </Offcanvas>
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-        <div className="container-fluid">
-          <Navbar.Brand onClick={handleShow}>
-            <i
-              className="bi bi-list"
-              style={{ fontSize: "2rem", color: "black" }}
-            ></i>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Navbar.Brand href="/">
-              {!PageName ? <img
-                src={banner}
-                width="100"
-                height="100%"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-              /> : <h5>{PageName}</h5>}
-
-
-            </Navbar.Brand>
-            <Nav className="me-auto">
-             {/*  <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-
-            </Nav>
-            <Nav>
-              <Nav.Link disabled>
-              {UserData?.rol}
-              </Nav.Link>
-              <NavDropdown title={<><i className="bi bi-person-circle text-dark icon-username"></i> {UserData?.user} </>} id="navbarScrollingDropdown">
-                <NavDropdown.Item href="/auth/porfile">Perfil</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Admin
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item
-                  href="#action5"
-                  onClick={() => {
-                    logout();
-                    handleLinkClick();
-                  }}
-                >
-                  Cerrar sesión
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link disabled>
-                Producción
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+    <React.Fragment>
+      <Button onClick={openDrawer} placeholder={"gfg"}>Open Drawer</Button>
+      <Drawer placeholder={"gfg"} open={open} onClose={closeDrawer} className="p-4">
+        <div className="mb-6 flex items-center justify-between">
+          <Typography variant="h5" color="blue-gray">
+            Material Tailwind
+          </Typography>
+          <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </IconButton>
         </div>
-      </Navbar>
-      <main>{children}</main>
-    </>
+        <Accordion open={open === 1}>
+        <AccordionHeader onClick={() => handleOpen(1)}>What is Material Tailwind?</AccordionHeader>
+        <AccordionBody>
+          We&apos;re not always in the position that we want to be at. We&apos;re constantly
+          growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
+          ourselves and actualize our dreams.
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 2}>
+        <AccordionHeader onClick={() => handleOpen(2)}>
+          How to use Material Tailwind?
+        </AccordionHeader>
+        <AccordionBody>
+          We&apos;re not always in the position that we want to be at. We&apos;re constantly
+          growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
+          ourselves and actualize our dreams.
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 3}>
+        <AccordionHeader onClick={() => handleOpen(3)}>
+          What can I do with Material Tailwind?
+        </AccordionHeader>
+        <AccordionBody>
+          We&apos;re not always in the position that we want to be at. We&apos;re constantly
+          growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
+          ourselves and actualize our dreams.
+        </AccordionBody>
+      </Accordion>
+      </Drawer>
+    </React.Fragment>
   );
 };
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,45 +1,9 @@
 import React from "react";
 import { BaseLayout } from "../components/BaseLayout";
-import { Card, Placeholder } from "react-bootstrap";
 export const Home = () => {
   return (
     <BaseLayout>
-      <div className="container" >
-        <div className="row">
-          {[
-            'Primary',
-            'Secondary',
-            'Success',
-            'Danger',
-            'Warning',
-            'Info',
-            'Light',
-            'Dark',
-          ].map((variant) => (
-            <Card
-              bg={variant.toLowerCase()}
-              key={variant}
-              text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-              style={{ width: '15rem', margin: 2, }}
-              className="mb-2"
-            >
-              <Card.Header>Header</Card.Header>
-              <Card.Body>
-                <Placeholder as={Card.Title} animation="glow">
-                  <Placeholder xs={6} />
-                </Placeholder>
-                <Placeholder as={Card.Text} animation="glow">
-                  <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-                  <Placeholder xs={6} /> <Placeholder xs={8} />
-                </Placeholder>
-              </Card.Body>
-            </Card>
-
-          ))}
-          <br />
-        </div>
-
-      </div>
+      
     </BaseLayout>
   );
 };
