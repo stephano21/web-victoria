159bd91559e98cfbfbe6e91d6fb0654b28693fff
Stephano <schang@apptelink.com>
2024-01-31 09:11:13 -0500

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BaseLayout } from "../components/BaseLayout";
-import { Card, Placeholder } from "react-bootstrap";
+import { Card, CardText, CardTitle, Placeholder } from "react-bootstrap";
 import { DataTable } from "../components/DataTable";
+import { title } from "process";
+import { text } from "stream/consumers";
 export const Home = () => {
   const columnNames = ['ID', 'First Name', 'Last Name', 'Age', 'Email'];
 
@@ -12,38 +14,49 @@ export const Home = () => {
       <div className="container" >
         <div className="row">
           {[
-            'Primary',
-            'Secondary',
-            'Success',
-            'Danger',
-            'Warning',
-            'Info',
-            'Light',
-            'Dark',
+            {
+              class: 'Primary',
+              area:'Usuarios',
+              icon: 'bi bi-people',
+              title: 'Usuarios registrados',
+              message:'2 usuarios nuevos',
+            },
+            {
+              class: 'success',
+              icon: 'bi bi-book',
+              area:'Lecturas',
+              title: 'Lecturas del mes',
+              message:'No se registrado ninguna lectura para este mes',
+            },
+            {
+              class: 'danger',
+              icon: 'bi bi-arrow-repeat',
+              area:'Sincronización',
+              title: 'Sincronizacion con Arable',
+              message:'La sincronizacion diaria ha fallado!',
+            }
+
           ].map((variant) => (
             <Card
-              bg={variant.toLowerCase()}
-              key={variant}
-              text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+              bg={variant.class.toLowerCase()}
+              key={variant.class}
+              text={variant.class.toLowerCase() === 'light' ? 'dark' : 'white'}
               style={{ width: '15rem', margin: 2, }}
               className="mb-2"
             >
-              <Card.Header>Header</Card.Header>
+              <Card.Header><i className={variant.icon}></i> {variant.area}</Card.Header>
               <Card.Body>
-                <Placeholder as={Card.Title} animation="glow">
-                  <Placeholder xs={6} />
-                </Placeholder>
-                <Placeholder as={Card.Text} animation="glow">
-                  <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-                  <Placeholder xs={6} /> <Placeholder xs={8} />
-                </Placeholder>
+                <CardTitle as={Card.Title} animation="glow">
+                  {variant.title}
+                </CardTitle>
+                <CardText>{variant.message}</CardText>
               </Card.Body>
             </Card>
 
           ))}
           <br />
         </div>
-       
+
       </div>
     </BaseLayout>
   );
