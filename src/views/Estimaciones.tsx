65709ec3d05eb17568d0e9a3b6f3b8b6807ec9df
Stephano <schang@apptelink.com>
2024-02-05 10:26:59 -0500

    Update Estimaciones.tsx

--- src/views/Estimaciones.tsx
+++ src/views/Estimaciones.tsx
@@ -8,6 +8,7 @@ import { Card, Modal, Spinner, Toast } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { GenericForm } from '../components/Form';
 import { usePlantaState } from '../states/PlantaState';
+import { DataTable } from '../components/DataTable';
 
 
 
@@ -16,6 +17,38 @@ export const Estimaciones = () => {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const columns = [
+    
+    {
+      dataField: 'Febrero',
+      text: 'Febrero',
+    },
+    {
+      dataField: 'Marzo',
+      text: 'Marzo',
+    },
+    {
+      dataField: 'Abril',
+      text: 'Abril',
+    },
+    {
+      dataField: 'Mayo',
+      text: 'Mayo',
+    },
+    {
+      dataField: 'Junio',
+      text: 'Junio',
+    }
+  ];
+  const data =[
+    {
+      'Febrero':171.5,
+      'Marzo':182.21,
+      'Abril':197.24,
+      'Mayo':201.15,
+      'Junio':231.44,
+    }
+  ];
   return (
     <BaseLayout PageName='Estimaciones'>
       <div className='container d-flex align-items-center justify-content-center'>
@@ -25,7 +58,7 @@ export const Estimaciones = () => {
               <Card.Body>
                 <Card.Title>Ultima producción</Card.Title>
                 <Card.Text>
-                  Se produjeron 385.54 qq
+                  Se produjeron 185.54 qq
                 </Card.Text>
                 <Button variant="primary" onClick={handleShow}>Estimaciones</Button>
               </Card.Body>
@@ -34,18 +67,18 @@ export const Estimaciones = () => {
           </div>
         </div>
       </div>
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} aria-labelledby="example-modal-sizes-title-lg" size="lg">
         <Modal.Header closeButton>
-          <Modal.Title>Febrero</Modal.Title>
+          <Modal.Title id="example-modal-sizes-title-lg">Valores estimados</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Se espera producir 405.5 qq</Modal.Body>
+        <Modal.Body>
+        <DataTable columnNames={columns} data={data}></DataTable>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            Close
+            Cerrar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
+         
         </Modal.Footer>
       </Modal>
     </BaseLayout>
