b1c9280d0771d88e73cb99e03d79a93d3aaec28e
Stephano <schang@apptelink.com>
2024-02-05 09:59:31 -0500

    Update Estimaciones.tsx

--- src/views/Estimaciones.tsx
+++ src/views/Estimaciones.tsx
@@ -4,7 +4,7 @@ import { CustomTable } from '../components/CustomTable';
 import { Endpoints } from '../api/routes';
 import { useRequest } from '../api/UseRequest';
 import { IPlantas } from '../interfaces/AuthInterface';
-import { Modal, Spinner, Toast } from 'react-bootstrap';
+import { Card, Modal, Spinner, Toast } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { GenericForm } from '../components/Form';
 import { usePlantaState } from '../states/PlantaState';
@@ -12,23 +12,42 @@ import { usePlantaState } from '../states/PlantaState';
 
 
 export const Estimaciones = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <BaseLayout PageName='Estimaciones'>
       <div className='container d-flex align-items-center justify-content-center'>
         <div className="row">
           <div className="col-md-12 text-center">
-            <Toast>
-              <Toast.Header>
-                <img src="holder.js/20x20?text=%20" className="btn btn-success rounded me-2" alt="" />
-                <strong className="me-auto">Bootstrap</strong>
-                <small>11 mins ago</small>
-              </Toast.Header>
-              <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-            </Toast>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Ultima producción</Card.Title>
+                <Card.Text>
+                  Se produjeron 385.54 qq
+                </Card.Text>
+                <Button variant="primary" onClick={handleShow}>Estimaciones</Button>
+              </Card.Body>
+            </Card>
+
           </div>
         </div>
       </div>
-
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Febrero</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Se espera producir 405.5 qq</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </BaseLayout>
   )
 }
